404f66c03ea72153e7db30b73734246117ac8e63
Mirhaydarov Radmir <Mirhaydarov@protonmail.com>
2020-03-05 16:06:01 +0500

    feat(main.js): add createTodo() function
    used .createElement() for  added todo tasks

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const todoInput = document.querySelector('.todo__input');
+const todoList = document.querySelector('.todo-list');
+const allBtn = document.querySelector('.all-btn');
+const activeBtn = document.querySelector('.active-btn');
+const completeBtn = document.querySelector('.complete-btn');
+const clearBtn = document.querySelector('.clear-btn');
+
+function createTodo() {
+    const todoContainer = document.createElement('li');
+    const todoItem = document.createElement('div');
+    const todoDesc = document.createElement('label');
+    const todoEdit = document.createElement('div');
+    const deleteBtn = document.createElement('button');
+}
